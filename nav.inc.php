de4eafff929e57e02d04352ca59c600e78b9bfa5
James Chen <jameschen@JamesdeMacBook-Pro.local>
2025-11-09 10:20:45 +0800

    Polish mobile sidenav visuals

--- nav.inc.php
+++ nav.inc.php
@@ -1,4 +1,15 @@
-<?php if(!isset($name)){$name=null;} ?>
+<?php
+if(!isset($name)){
+  $name = null;
+}
+
+$nav_display_name = '';
+if(isset($park_info['cname']) && $park_info['cname']!==''){
+  $nav_display_name = $park_info['cname'];
+} elseif(!empty($name)){
+  $nav_display_name = ucfirst($name);
+}
+?>
       <div class="navbar-fixed">
         <nav>
           <div class="nav-wrapper nav-header">
@@ -27,80 +38,48 @@
         </nav>
       </div>
 <?php
-    // 雪場 ＆ 教練  手機選單個別處理
-    if(strstr($_SERVER['PHP_SELF'],'routing') || strstr($_SERVER['PHP_SELF'],'park') ){
-      //$_SERVER['PHP_SELF'].$target;
+    $is_park_context = (strstr($_SERVER['PHP_SELF'],'routing') || strstr($_SERVER['PHP_SELF'],'park'));
 ?>
       <ul class="sidenav" id="mobile-nav">
-        <div class="row">
-          <a href="#" class="sidenav-close">
-            <img src="/assets/images/logo-skidiy.png?v251026" alt=""  class="logo">
+        <li class="sidenav-header">
+          <a href="/" class="sidenav-close logo-link">
+            <img src="/assets/images/logo-skidiy.png?v251026" alt="SKIDIY" class="logo">
           </a>
-        </div>
-        <?php
-          $nav_display_name = '';
-          if(isset($park_info['cname']) && $park_info['cname']!==''){
-            $nav_display_name = $park_info['cname'];
-          } elseif(!empty($name)) {
-            $nav_display_name = ucfirst($name);
-          }
-        ?>
-        <?php if(!empty($nav_display_name)){ ?><li><?=$nav_display_name?></li><?php } ?>
-        <li class="subnav">
-        <!--
-          <p><i class="material-icons">place</i></p>
-          <p align="center" class="resort-name2" onclick="$('#childnav').toggle();"><?=$section_content['cname']?> <?=$name?></p>
-          -->
-          <ul id="childnav">
-          <?php
-                if(isset($SECTION_HEADER) && is_array($SECTION_HEADER)){
-                  reset($SECTION_HEADER);
-                  foreach($SECTION_HEADER as $key => $val){
-                    if($key == 'all') continue; // Skip "完整閱讀"
-                    // Use anchor links for park.php, route links for routing.php
-                    if(strstr($_SERVER['PHP_SELF'],'park')){
-                      echo '<a href="#' . $key . '" class="tab"><li>'.$val.'</li></a>';
-                    } else {
-                      echo '<a href="https://'.domain_name.'/'.$name.'/'.$key.'" class="tab"><li>'.$val.'</li></a>';
-                    }
-                  }
-                }
-          ?>
-          </ul>
+          <?php if(!empty($nav_display_name)){ ?>
+            <p class="sidenav-resort"><?=$nav_display_name?></p>
+          <?php } ?>
         </li>
 
-        <?php if(strstr($_SERVER['PHP_SELF'],'park')){ ?>
-        <li><a href="/">其他雪場</a></li>
-        <li><a href="https://booking.diy.ski/schedule?action=login&redirect=/schedule">登入</a></li>
-        <li><a href="/instructorList.php">教練團隊</a></li>
-        <li><a href="/articleList.php">相關文章</a></li>
-        <li><a href="/schedule.php">預訂課程</a></li>
-        <?php } else { ?>
-        <li><a href="../">其他雪場</a></li>
-        <li><a href="https://booking.diy.ski/schedule?action=login&redirect=/schedule">登入</a></li>
-        <li><a href="../instructorList.php">教練團隊</a></li>
-        <li><a href="../articleList.php">相關文章</a></li>
-        <li><a href="https://booking.diy.ski">預訂課程</a></li>
+        <?php if($is_park_context && isset($SECTION_HEADER) && is_array($SECTION_HEADER)){ ?>
+          <li class="sidenav-label">雪場導覽</li>
+          <?php
+            foreach($SECTION_HEADER as $key => $val){
+              if($key == 'all') continue;
+              if(strstr($_SERVER['PHP_SELF'],'park')){
+                echo '<li><a class="sidenav-link sidenav-close" href="#'.$key.'">'.$val.'</a></li>';
+              } else {
+                echo '<li><a class="sidenav-link" href="https://'.domain_name.'/'.$name.'/'.$key.'">'.$val.'</a></li>';
+              }
+            }
+          ?>
+          <li class="sidenav-divider"></li>
         <?php } ?>
-        <li><a href="https://faq.diy.ski" target="_blank">常見問題</a></li>
-        <li><a href="mailto:service@diy.ski">聯絡我們</a></li>
+
+        <li class="sidenav-label">SKIDIY</li>
+        <?php if($is_park_context){ ?>
+          <li><a class="sidenav-link sidenav-close" href="/">其他雪場</a></li>
+          <li><a class="sidenav-link" href="/instructorList.php">教練團隊</a></li>
+          <li><a class="sidenav-link" href="/articleList.php">相關文章</a></li>
+          <li><a class="sidenav-link" href="/schedule.php">預訂課程</a></li>
+        <?php } else { ?>
+          <li><a class="sidenav-link sidenav-close" href="/">雪場資訊</a></li>
+          <li><a class="sidenav-link" href="/instructorList.php">教練團隊</a></li>
+          <li><a class="sidenav-link" href="/articleList.php">相關文章</a></li>
+          <li><a class="sidenav-link" href="/schedule.php">預訂課程</a></li>
+        <?php } ?>
+        <li><a class="sidenav-link" href="https://booking.diy.ski/schedule?action=login&redirect=/schedule">登入</a></li>
+        <li><a class="sidenav-link" href="https://faq.diy.ski" target="_blank">常見問題</a></li>
+        <li><a class="sidenav-link" href="mailto:service@diy.ski">聯絡我們</a></li>
       </ul>
-<?php
-    }else{
-?>
-      <ul class="sidenav" id="mobile-nav" style="z-index:999999999;">
-        <div class="row">
-            <a href="/" class="sidenav-close"> <img src="/assets/images/logo-skidiy.png?v251026" alt=""  class="logo"></a>
-        </div>
-        <li><a href="/">雪場資訊</a></li>
-        <li><a href="/instructorList.php">教練團隊</a></li>
-        <li><a href="/articleList.php">相關文章</a></li>
-        <li><a href="/schedule.php">預訂課程</a></li>
-        <li><a href="https://faq.diy.ski" target="_blank">常見問題</a></li>
-        <li><a href="mailto:service@diy.ski">聯絡我們</a></li>
-      </ul>
-<?php
-    }
-?>
 
 
