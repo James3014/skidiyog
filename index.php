8e293a08f5d435a4703373beddefada7fbbed6ab
James Chen <jameschen@JamesdeMacBook-Pro.local>
2025-11-10 13:54:12 +0800

    Enforce preview token guard and preview sitemap

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
-// 重定向到雪場列表頁面（網站首頁）
-header('Location: /parkList.php');
+require('includes/sdk.php');
+
+$renderParam = defined('SKID_PREVIEW_RENDER_PARAM') ? SKID_PREVIEW_RENDER_PARAM : 'render';
+
+$target = '/parkList.php';
+$query = [];
+if (!empty($_GET['preview_token'])) {
+    $query['preview_token'] = $_GET['preview_token'];
+}
+if (!empty($_GET[$renderParam])) {
+    $query[$renderParam] = $_GET[$renderParam];
+}
+$qs = $query ? '?' . http_build_query($query) : '';
+header('Location: ' . $target . $qs);
 exit;
